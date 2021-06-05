b0afbeabe8d474d2756d9fb4df562c41f1ae236b
Nick Bünger <buenger.nick@gmail.com>
2021-06-05 21:01:41 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,113 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"govee adapter settings": {
+		"en": "Adapter settings for govee",
+		"de": "Adaptereinstellungen für govee",
+		"ru": "Настройки адаптера для govee",
+		"pt": "Configurações do adaptador para govee",
+		"nl": "Adapterinstellingen voor govee",
+		"fr": "Paramètres d'adaptateur pour govee",
+		"it": "Impostazioni dell'adattatore per govee",
+		"es": "Ajustes del adaptador para govee",
+		"pl": "Ustawienia adaptera dla govee",
+		"zh-cn": "govee的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	},
+	"myColor": {
+		"en": "myColor",
+		"de": "meineColor",
+		"ru": "Мой цвет",
+		"pt": "minhaCor",
+		"nl": "mijnKleur",
+		"fr": "maCouleur",
+		"it": "mioColore",
+		"es": "miColor",
+		"pl": "mójKolor",
+		"zh-cn": "我的颜色"
+	},
+	"myColor_tooltip": {
+		"en": "Description of\nmyColor",
+		"de": "Beschreibung von\nmyColor",
+		"ru": "Описание\nmyColor",
+		"pt": "Descrição de\nmyColor",
+		"nl": "Beschrijving van\nmyColor",
+		"fr": "Description de\nmyColor",
+		"it": "Descrizione di\nmyColor",
+		"es": "Descripción de\nmyColor",
+		"pl": "Opis\nmyColor",
+		"zh-cn": "\nmyColor的描述"
+	},
+	"htmlText": {
+		"en": "htmlText",
+		"de": "htmlText",
+		"ru": "htmlText",
+		"pt": "htmlText",
+		"nl": "htmlText",
+		"fr": "htmlText",
+		"it": "htmlText",
+		"es": "htmlText",
+		"pl": "htmlText",
+		"zh-cn": "htmlText"
+	},
+	"group_extraMyset": {
+		"en": "extraMyset",
+		"de": "extraMyset",
+		"ru": "extraMyset",
+		"pt": "extraMyset",
+		"nl": "extraMyset",
+		"fr": "extraMyset",
+		"it": "extraMyset",
+		"es": "extraMyset",
+		"pl": "extraMyset",
+		"zh-cn": "extraMyset"
+	},
+	"extraAttr": {
+		"en": "extraAttr",
+		"de": "extraAttr",
+		"ru": "extraAttr",
+		"pt": "extraAttr",
+		"nl": "extraAttr",
+		"fr": "extraAttr",
+		"it": "extraAttr",
+		"es": "extraAttr",
+		"pl": "extraAttr",
+		"zh-cn": "extraAttr"
+	},
+	"Instance": {
+		"en": "Instance",
+		"de": "Instanz",
+		"ru": "Инстанция",
+		"pt": "Instância",
+		"nl": "Instantie",
+		"fr": "Instance",
+		"it": "Esempio",
+		"es": "Instancia",
+		"pl": "Instancja",
+		"zh-cn": "例"
+	}
+};
